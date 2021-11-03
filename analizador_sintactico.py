e9f483959574604e2b7f868e4ec66cbd51b64d7b
TheNanfe <nanrelo@gmail.com>
2021-11-03 10:47:25 -0300

    cambiando el nombre de la funcion a next_token

--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -23,7 +23,7 @@ class SyntaxCheck():
 		for x in self.fuente.readlines():
 			self.input_file.write(re.sub(r"[\t\n]*", "", x))
 
-	def next_position(self, increment):
+	def next_token(self, increment):
 		self.absolute_position = self.absolute_position + increment
 		
 	def tokens_array(self):
@@ -33,48 +33,48 @@ class SyntaxCheck():
 
 	def llaves(self):
 		if not self.tokens[self.absolute_position] == 'l_llave': return False
-		self.next_position(1)
+		self.next_token(1)
 		self.cuerpo()
 		while self.tokens[self.absolute_position] == 'coma': 
-			self.next_position(1)
+			self.next_token(1)
 			self.value()
 			if self.tokens[self.absolute_position] == 'r_corchete': break
 		if not self.tokens[self.absolute_position] == 'r_llave': raise SyntaxError
-		self.next_position(1)
+		self.next_token(1)
 		return True
 
 	def corchetes(self):
 		if not self.tokens[self.absolute_position] == 'l_corchete': return False
-		self.next_position(1)
+		self.next_token(1)
 		self.llaves()
 		self.corchetes()
 		self.cuerpo()
 		while self.tokens[self.absolute_position] != 'l_corchete' and self.tokens[self.absolute_position] != 'dos_puntos' : 
 			if self.tokens[self.absolute_position] == 'coma':
-				self.next_position(1)
+				self.next_token(1)
 			self.value()
 			if self.tokens[self.absolute_position] == 'r_corchete': break
 		if not self.tokens[self.absolute_position] == 'r_corchete': raise SyntaxError
-		self.next_position(1)
+		self.next_token(1)
 		return True
 	
 	def value(self):
 		if self.corchetes(): return True
 		if self.llaves(): return True
 		if self.tokens[self.absolute_position] == 'number': 
-			self.next_position(1)
+			self.next_token(1)
 			return True
 		if self.tokens[self.absolute_position] == 'string': 
-			self.next_position(1)
+			self.next_token(1)
 			return True
 		if self.tokens[self.absolute_position] == 'pr_false': 
-			self.next_position(1)
+			self.next_token(1)
 			return True
 		if self.tokens[self.absolute_position] == 'pr_true': 
-			self.next_position(1)
+			self.next_token(1)
 			return True
 		if self.tokens[self.absolute_position] == 'pr_null': 
-			self.next_position(1)
+			self.next_token(1)
 			return True
 		if self.tokens[self.absolute_position] == 'coma': 
 			return True
@@ -83,10 +83,10 @@ class SyntaxCheck():
 	def cuerpo (self):
 		if not self.tokens[self.absolute_position] == 'string': return False
 		if not (self.tokens[self.absolute_position + 1] == 'dos_puntos' or  self.tokens[self.absolute_position + 1] == 'coma'): raise SyntaxError
-		self.next_position(2)
+		self.next_token(2)
 		if not self.value(): raise SyntaxError
 		if self.tokens[self.absolute_position] == 'coma':
-			self.next_position(1)
+			self.next_token(1)
 			if not self.cuerpo() and not self.value(): raise SyntaxError
 		return True
 		
