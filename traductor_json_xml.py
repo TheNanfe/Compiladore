a3f2f25510e053e6eba29a843fa89cde6ec77190
TheNanfe <nanrelo@gmail.com>
2021-11-18 15:45:31 -0300

    Correccion de algunnos clousures tags

--- traductor_json_xml.py
+++ traductor_json_xml.py
@@ -2,6 +2,7 @@ from analizador_sintactico import SyntaxCheck
 from lexer.lexer import Lexer
 
 class Translate():
+	
 	def __init__ (self, input_file, lexer_output_path, traductor_output=None):
 		self.input_file = input_file
 		self.lexer_output_path = lexer_output_path
@@ -15,25 +16,30 @@ class Translate():
 		self.traductor_output = traductor_output
 		self.separated_simbols = []
 		self.lexer_simbols = []
+
 	
 	def lexer_tokens_array(self):
 		self.lexer_output = open(lexer_output_path, 'r')
 		self.lexer_simbols = self.lexer_output.read().split()
 		self.lexer_output.close()
 
+
 	def tokens_array(self):
 		self.input_file = open('lexer/input.txt', 'r')
 		self.tokens = self.input_file.read()
 		self.input_file.close()
 
+
 	def next_token(self, increment):
 		self.absolute_position = self.absolute_position + increment
 		self.token = self.tokens[self.absolute_position]
+
 		
 	def tranlsate_next_token(self, increment):
 		self.absolute_position = self.absolute_position + increment
 		self.token = self.separated_simbols[self.absolute_position]
 		self.lexer_token = self.lexer_simbols[self.absolute_position]
+
 	
 	def increase_tabs(self, increment):
 		self.tabs_numbers = self.tabs_numbers + increment
@@ -55,16 +61,22 @@ class Translate():
 		while self.absolute_position < len(self.tokens)-1:
 			if self.token == '{':
 				self.separated_simbols.append('{')
+
 			if self.token == '}':
 				self.separated_simbols.append('}')
+
 			if self.token == '[':
 				self.separated_simbols.append('[')
+
 			if self.token == ']':
 				self.separated_simbols.append(']')
+
 			if self.token == ',':
 				self.separated_simbols.append(',')
+
 			if self.token == ':':
 				self.separated_simbols.append(':')
+
 			if self.token == '"':
 				inner_simbols = []
 				while True:
@@ -77,6 +89,7 @@ class Translate():
 						break
 					else:
 						inner_simbols.append(self.token)
+						
 			if self.token.isdigit():
 				flag = 1
 				inner_simbols = []
@@ -88,6 +101,7 @@ class Translate():
 						numbers = ''.join(inner_simbols)
 						self.separated_simbols.append(numbers)
 						break
+
 			if self.token == 't':
 				self.separated_simbols.append('true')
 			elif self.token == 'f':
@@ -99,14 +113,15 @@ class Translate():
 				self.next_token(1)
 			else:
 				flag = 0
+
 		self.separated_simbols.append('}')
 
 	def translating(self):
-		bandera_primer_caracter = 0
-		bandera_cierre_llave =  0
-		bandera_cierre_corchete = 0
+		closures_llave = []
+		closures_corchete = []
 		self.separating_simbols()
 		self.absolute_position = 0
+
 		while self.absolute_position != len(self.separated_simbols)-1:
 			
 			if self.lexer_token == 'l_llave':
@@ -119,21 +134,24 @@ class Translate():
 				self.write_file(string)
 				self.increase_tabs(1)
 
+			
 			if self.lexer_token == 'r_llave':
-				if bandera_cierre_llave == 1:
-					self.increase_tabs(-1)
-					self.write_file(token_cierre)
-					bandera_cierre_llave = 0
+				if closures_llave:
+					if closures_llave[-1:][0][-1:][0] == self.tabs_numbers:
+						self.increase_tabs(-1)
+						self.write_file(closures_llave[-1:][0][0])
+						closures_corchete = closures_llave[:-1]
 				else:
 					self.increase_tabs(-1)
 					string = "</item>"
 					self.write_file(string)
 
 			if self.lexer_token == 'r_corchete':
-				if bandera_cierre_corchete == 1:
-					self.increase_tabs(-1)
-					self.write_file(token_cierre)
-					bandera_cierre_corchete = 0
+				if closures_corchete:
+					if closures_corchete[-1:][0][-1:][0] == self.tabs_numbers:
+						self.increase_tabs(-1)
+						self.write_file(closures_corchete[-1:][0][0])
+						closures_corchete = closures_corchete[:-1]
 				else:	
 					self.increase_tabs(-1)
 					string = "</item>"
@@ -163,9 +181,9 @@ class Translate():
 						string = ["<",tag,">"]
 						self.write_file(string)
 						self.increase_tabs(1)
-						bandera_cierre_llave = 1
 						string.insert(1, '/')
 						token_cierre = string
+						closures_llave.append([''.join(token_cierre), self.tabs_numbers])
 						if bandera_primer_caracter == 0:
 							primer_caracter = token_cierre
 							bandera_primer_caracter = 1
@@ -175,22 +193,15 @@ class Translate():
 						string = ["<",tag,">"]
 						self.write_file(string)
 						self.increase_tabs(1)
-						bandera_cierre_corchete = 1
 						string.insert(1, '/')
 						token_cierre = string
-						if bandera_primer_caracter == 0:
-							primer_caracter = token_cierre
-							bandera_primer_caracter = 1
-			self.tranlsate_next_token(1)
-			if self.absolute_position == len(self.separated_simbols)-1:
-				self.increase_tabs(-1)
-				self.write_file(primer_caracter)
+						closures_corchete.append([''.join(token_cierre), self.tabs_numbers])
 
-		
-	
+			self.tranlsate_next_token(1)
 
 
 if __name__ == '__main__':
+	
 	try:
 		lexer = Lexer().get_tokens()
 	except:
@@ -209,16 +220,12 @@ if __name__ == '__main__':
 		print("//////////////////////////////////////////////////////////////////////////////////////\n")
 		raise
 
-
-
 	traductor_output = open('traductor_output.xml', 'w')
 	input = open('lexer/input.txt', 'r')
 	lexer_output_path = 'input.txt'
 	translate = Translate(input, lexer_output_path, traductor_output)
 	translate.translating()
 	input.close()
-
-
 	print(":::::::::::::::::::::::::::::::::::::::::  Traductor  ::::::::::::::::::::::::::::::::::::::")
 	print(".\nSe ha generado un archivo .xml en esta misma carpeta.\n.")
 	print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
